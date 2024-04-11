b4f53b9db6ae36900e47be9177d0a63e568f27c5
Kacper Walichnowski <kacperw112@wp.pl>
2024-04-11 20:00:13 +0200

    Add Home Controller and template

--- src/main/java/org/example/springmvcsecurity/SpringMvcSecurityApplication.java
+++ src/main/java/org/example/springmvcsecurity/SpringMvcSecurityApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMvcSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcSecurityApplication.class, args);
+    }
 
 }
